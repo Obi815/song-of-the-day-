3d289ae620ca7e5dc83ddaea149a8c450214f179
Obinna Ezeka <obinnae815@gmail.com>
2026-03-18 15:29:08 -0700

    song object is created

--- server.js
+++ server.js
@@ -2,6 +2,28 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
+//Create Song of the Day Object 
+const song = {
+    'larry june':{
+        'song': "Let's Drive to Vegas",
+        'album': 'Out The Trunk',
+        'released': 2019
+    },
+    'steve lacey': {
+        'song': 'Mercury',
+        'album': 'Gemini Rights',
+        'released': 2022
+    },
+    'nipsey hussle': {
+        'song': 'Face the World',
+        'album': 'Crenshaw',
+        'released': 2013
+    }
+}
+
 app.listen(process.env.PORT || PORT, () => {
     console.log(`The serving is running on on port ${PORT}! You better go catch it!`)
+})
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html');
 })
