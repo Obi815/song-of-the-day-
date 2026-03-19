51758851fd2ffeb121bd449800d3e204d9791689
Obinna Ezeka <obinnae815@gmail.com>
2026-03-18 17:24:06 -0700

    fixed the songs api

--- server.js
+++ server.js
@@ -3,21 +3,21 @@ const app = express()
 const PORT = 8000
 
 //Create Song of the Day Object 
-const song = {
-    'larry june':{
-        'song': "Let's Drive to Vegas",
+const songs = {
+    "Let's Drive to Vegas": {
         'album': 'Out The Trunk',
+        'artist': 'Larry June',
         'released': 2019
     },
-    'steve lacey': {
-        'song': 'Mercury',
-        'album': 'Gemini Rights',
-        'released': 2022
-    },
-    'nipsey hussle': {
-        'song': 'Face the World',
+    "face the world": {
         'album': 'Crenshaw',
+        'artist': 'Nipsey Hussle',
         'released': 2013
+    },
+    'mercury': {
+        'album': 'Gemini Rights',
+        'artist': 'Steve Lacey',
+        'date': 2022
     }
 }
 
@@ -25,8 +25,13 @@ app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html'); // Getting the html file and responding with it to the browser
 })
 
-app.get('/api', (request, response) =>{
-    response.json(song)
+app.get('/api/:name', (request, response) => {
+    const songName = request.params.name.toLowerCase()
+    if(rappers[songName]){
+        response.json(songs[songName])
+    }else{
+        response.json(songs['unknown'])
+    }
 })
 
 app.listen(process.env.PORT || PORT, () => { // Telling express to listen to hosting site or local port 
