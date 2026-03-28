3c435e0ffaa940968b6a4d3cf268e3f8bad74ceb
Obinna Ezeka <obinnae815@gmail.com>
2026-03-27 17:34:31 -0700

    trying to get the album covers

--- assets/main.js
+++ assets/main.js
@@ -2,7 +2,24 @@ document.querySelector('#submit').addEventListener('click', songSearch)
 
 async function songSearch() {
     const query = document.getElementById('artistName').value;
-    const res = await fetch(`https://itunes.apple.com/search?term=${encodeURIComponent(query)}&entity=song&limit=5`);
+
+    const res = await fetch(`https://itunes.apple.com/search?term=${encodeURIComponent(query)}&entity=song&limit=10`);
     const data = await res.json()
-    console.log(data)
+
+    const results = document.getElementById('results')
+
+    // clear old results
+    results.innerHTML = ''
+
+    data.results.forEach(song => {
+        results.innerHTML += `
+            <div>
+                <h2>${song.trackName}</h2>
+                <p>${song.artistName}</p>
+                <img src="${song.artworkUrl100}">
+            </div>
+        `
+    })
+
+    document.getElementById('artistName').value = ''
 }
